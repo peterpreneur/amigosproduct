a1b12f2099c796cb45662158f0f6b325f8ce19b7
Peter S <peterpreneur@gmail.com>
2026-09-22 12:54:56 -0500

    init

---
+++ src/main/java/com/peterpreneur/amigosproduct/AmigosproductApplication.java
@@ -0,0 +1,13 @@
+package com.peterpreneur.amigosproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosproductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peterpreneur/amigosproduct/AmigosproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterpreneur.amigosproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
